33d4a2f514036fb722ef721b750c94bb0f73a7b8
CarolinaaCabral <carolinacabral@outlook.com.br>
2026-08-15 12:40:41 -0300

    feat: adiciona classe Ninja

---
+++ Desafio1/Ninja.java
@@ -0,0 +1,22 @@
+package NivelIntermediario.Desafios.Desafio1;
+
+public class Ninja {
+    String nome;
+    int idade;
+    String missao;
+    String nivelDificuldade;
+    String statusMissao;
+
+
+    public void mostrarInformacoes() {
+        System.out.println("=== Informações do Ninja ===");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Missao: " + missao);
+        System.out.println("Nivel dificuldade: " + nivelDificuldade);
+        System.out.println("Status: " + statusMissao);
+        System.out.println("=============================");
+    }
+
+
+}
